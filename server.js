af07f89018208f481a3539dc25bf519fe2d59e8d
CorcoPrkl <akarppanen@hotmail.com>
2014-10-27 02:41:54 +0200

    restart

--- server.js
+++ server.js
@@ -1,159 +1,88 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+var mysql   = require('mysql');
 var fs      = require('fs');
+var irc		= require('irc');
+ 
+var mysqlconn = mysql.createConnection({
+host : process.env.OPENSHIFT_MYSQL_DB_HOST,
+port : process.env.OPENSHIFT_MYSQL_DB_PORT,
+user : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+database : 'ircbot',
+});
 
+mysqlconn.connect();
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
 
-    //  Scope.
-    var self = this;
+var bot = new irc.Client('fi.quakenet.org', 'Cobotti', {
+    channels: ['#ircbot-testi'],//, '#ludns13', '#jumala 667'],
+    port: 6667,
+    debug: true,
+	autoConnect: true,
+	floodProtection: true,
+	retryDelay: 60000,
+});
 
+bot.addListener('message', function(from, to, message) {
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+var subMessage = message.split(" ");
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+if (subMessage[0] == "!def") {
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+var querystr = ("SELECT * FROM quotes WHERE id = '"+subMessage[1]+"' LIMIT 1;");
+mysqlconn.query(querystr, function(err, rows) {
+	if (err) throw err;
+	if (rows.length == 0) bot.say(to, "Quote '"+subMessage[1]+"' not found");
+	if (rows.length > 0) for (var i in rows) {
+	bot.say(to, rows[i].id+ ": '"+rows[i].quote+"'");	
+	}
+	});
+}
 
+if (subMessage[0] == "!defadd") {
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+	var querystr = ("SELECT * FROM quotes WHERE id = '"+subMessage[1]+"';");
+	mysqlconn.query(querystr, function(err, rows) {
+	if (rows.length > 0) bot.say(to, "Duplicate entry, try 'def! "+subMessage[1]+"'");
+	else {
+	var joinedMessage = subMessage[2]+" ";
+	for (var i = 3; i < subMessage.length; i++)
+	{
+	joinedMessage += subMessage[i]+" ";
+	}
+	joinedMessage = joinedMessage.slice(0, -1);
+	var querystr = ("INSERT INTO quotes(id, quote) VALUES ('"+subMessage[1]+"','"+joinedMessage+"') ON DUPLICATE KEY UPDATE id=id,quote=quote;");
+	
+	mysqlconn.query(querystr, function(err, rows) {
+	if (err) throw err;
+	if  (rows.affectedRows > 0) bot.say(to, "Quote added: !def "+subMessage[1]+ ", " +joinedMessage);
+	});
+	}
+	
+	});
+	}
+	
+if (subMessage[0] == "!defrem") {
+var querystr = ("DELETE FROM quotes WHERE id = '"+subMessage[1]+"';");
+mysqlconn.query(querystr, function(err, rows){
+if (err) throw err;
+if (rows.affectedRows > 0) bot.say(to, "Quote '"+subMessage[1]+"' removed!");
+if (rows.affectedRows == 0) bot.say(to, "Quote '"+subMessage[1]+"' not found!");
+});
+}
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+if (subMessage[0] == "!help") {
+bot.say(to, "!def NAME to read a quote, !defadd NAME QUOTE to add a new one, !defrem NAME to remove a quote. Have fun!");
+}
 
+if (subMessage[0] == "!list") {
+bot.say(to, "http://ircbot-corcoder.rhcloud.com");
+}
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+});

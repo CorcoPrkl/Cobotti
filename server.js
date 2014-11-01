c0460ec6d7128efe4e029946b933d40db06cb04f
CorcoPrkl <akarppanen@hotmail.com>
2014-11-01 20:46:27 +0200

    added dynamic channel list

--- server.js
+++ server.js
@@ -27,10 +27,11 @@ app.get('/api', function(req, res) {
   });
   });
 
-var ircChannels;
+var ircChannels = "";
 var querystr = 'SELECT * FROM channels;';
 mysqlconn.query(querystr, function(err, rows, fields) {
-for (var i in rows) {
+if (rows.length == 0) (console.log("nyt perkele");
+else for (var i in rows) {
 ircChannels += ("'"+rows[i].channel+"',");
 }
 });

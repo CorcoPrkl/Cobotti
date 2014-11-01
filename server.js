7b6b0d905f29df8979fe2982d231248abb60ae36
CorcoPrkl <akarppanen@hotmail.com>
2014-11-01 20:20:53 +0200

    added dynamic channel list

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ ircChannels += rows[i].channel+",";
 }
 });
 console.log("channels: "+ircChannels);
+ircChannels = "'#ircbot-testi'";
 
 ircChannels = ircChannels.slice(0,-1);
 var bot = new irc.Client('fi.quakenet.org', 'Cobotti', {
